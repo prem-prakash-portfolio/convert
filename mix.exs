c5f0d2cd838016f5a0b8f70568ce5008d581fe53
Prem Prakash <prakash2ji@gmail.com>
2024-01-13 11:24:40 -0300

    ex_machina install

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule CurrencyConverter.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ex_machina, "~> 2.7.0", only: :test}
     ]
   end
 
