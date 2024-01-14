a33c5027d68dcdb97be3feca4c2cfe35d590119b
Prem Prakash <prakash2ji@gmail.com>
2024-01-14 15:45:07 -0300

    formatter line lenght

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 [
+  line_length: 120,
   import_deps: [:ecto, :ecto_sql, :phoenix],
   subdirectories: ["priv/*/migrations"],
   inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}", "priv/*/seeds.exs"]
